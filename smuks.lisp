a8f3c19325d86a61492d2f6b221cb721bebcb95d
bmiww <bmiww@bky.one>
2024-03-18 01:43:16 +0100

    Going crazy and implementing my own wayland wire protocol parser

--- smuks.lisp
+++ smuks.lisp
@@ -27,12 +27,34 @@
 		     (stream (unix-sockets:unix-socket-stream client)))
 		(print "CLIENT CONNECTED")
 		(bt:make-thread (lambda ()
-				  (loop for input = (print (read-line stream))
-					until (not input)
-					do (print input))))))))
+				  (loop until *smuks-exit*
+					do
+					   (let ((object-id (make-array 4 :initial-element nil))
+						 (message-size (make-array 2 :initial-element nil))
+						 (opcode (make-array 2 :initial-element nil))
+						 (payload nil))
+					     (loop for i from 0 below 4
+						   do (setf (aref object-id i) (read-byte stream)))
+					     (loop for i from 0 below 2
+						   do (setf (aref message-size i) (read-byte stream)))
+					     (loop for i from 0 below 2
+						   do (setf (aref opcode i) (read-byte stream)))
+					     (setf payload (make-array (bytes-to-num message-size) :initial-element nil))
+					     (loop for i from 0 below (bytes-to-num message-size)
+						   do (setf (aref payload i) (read-byte stream)))
+
+					     ;; Discard extra bytes - since wayland messages are always 32-bit aligned
+					     (when (> (mod (bytes-to-num message-size) 4) 0)
+					       (loop for i from 0 below (- 4 (mod (bytes-to-num message-size) 4))
+						     do (read-byte stream)))
+
+					     (format *standard-output* "ID: ~a, SIZE: ~a, OPCODE: ~a, PAY: ~a~%"
+						     (bytes-to-num object-id) (bytes-to-num message-size)
+						     (bytes-to-num opcode) payload)))
+				  (print "Exiting client")
+				  (unix-sockets:close-unix-socket client)))))))
 
   (setf (uiop/os:getenv "WAYLAND_DISPLAY") *socket-file*)
-
 
   (thread:make-thread
    (lambda ()
@@ -50,6 +72,20 @@
 
 (defun magic ()
   (sleep 1))
+
+
+;; ██╗   ██╗████████╗██╗██╗
+;; ██║   ██║╚══██╔══╝██║██║
+;; ██║   ██║   ██║   ██║██║
+;; ██║   ██║   ██║   ██║██║
+;; ╚██████╔╝   ██║   ██║███████╗
+;;  ╚═════╝    ╚═╝   ╚═╝╚══════╝
+
+(defun bytes-to-num (byte-arr)
+  (let ((num 0))
+    (dotimes (i (length byte-arr))
+      (setf (ldb (byte 8 (* i 8)) num) (aref byte-arr i)))
+    num))
 
 
 ;; ███████╗ ██████╗  ██████╗██╗  ██╗███████╗████████╗
