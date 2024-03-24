ba0360f841f1a64e7309ec1e8aea2060b384f66a
bmiww <bmiww@bky.one>
2024-03-24 19:24:43 +0100

    Wayland generator madness

--- wayland-generator.lisp
+++ wayland-generator.lisp
@@ -12,27 +12,50 @@
 (defun req-name (request) (format nil "req-~a" (name request)))
 (defun symbolize-event (event) (read-from-string (ev-name event)))
 (defun do-arg (arg) (name arg))
+
 (defun do-event (interface event)
   `(defmethod ,(ev-name event) ((obj ,interface) ,@(mapcar 'do-arg (args event)))))
-
-;; TODO: Embed a request opcode as a default value in the args?
-;; Do these "request" methods even really make sense?
-
-(defun do-request (interface request)
-  `(defmethod ,(req-name request) ((obj ,interface) ,@(mapcar 'do-arg (args request)))))
 (defun do-event-opcode-matchers (interface events)
   `(defmethod match-event-opcode ((obj ,interface) opcode)
      (nth opcode '(,@(mapcar 'symbolize-event events)))))
 
+(defun do-request (interface request)
+  `(defmethod ,(req-name request) ((obj ,interface) ,@(mapcar 'do-arg (args request)))))
+(defun do-request-opcode-matchers (interface requests)
+  `(defmethod match-request-opcode ((obj ,interface) opcode)
+     (nth opcode '(,@(mapcar 'symbolize-event requests)))))
+
+(defun do-initializer (interface)
+  `(defmethod initialize-instance ":after" ((obj ,interface) &key)
+     (setf (gethash (id obj) wl:*objects*) obj)))
+
 (defun do-interface (interface)
   `(progn
-     (defclass ,(name interface) "()"
+     (defpackage ,(format nil ":~a" (name interface)))
+     (in-package ,(format nil ":~a" (name interface)))
+
+     ;; TODO: This could probably move the client to the wl-object thing
+     (defclass ,(name interface) "(wl:wl-object)"
        ("(client :initarg :client :accessor client)"))
      ,@(mapcar (lambda (event) (do-event (name interface) event)) (events interface))
      ,@(mapcar (lambda (request) (do-request (name interface) request)) (requests interface))
-     ,(do-event-opcode-matchers (name interface) (events interface))))
+     ,(do-event-opcode-matchers (name interface) (events interface))
+     ,(do-request-opcode-matchers (name interface) (requests interface))
+     ;; ,(do-initializer (name interface))))
+     ))
 
-(defun gen-lisp-code (protocol) `(progn ,@(mapcar 'do-interface protocol)))
+;; TODO: The whole packages thing is problematic. Remove it.
+;; TODO: It seems that you might also need to prefix the methods
+;; since their generic definitions are conflicting.
+(defun gen-lisp-code (protocol)
+  `(progn
+     (defpackage ":wl" "(:use #:cl)")
+     (in-package ":wl")
+     (defvar *objects* "(make-hash-table :test 'eq)")
+     (defclass wl-object "()" "((id :initarg :id :accessor id))")
+     (export '*objects* 'wl-object)
+     ,(do-initializer "wl-object")
+     ,@(mapcar 'do-interface protocol)))
 
 
 (defun generate-wayland-classes (package xml-file)
