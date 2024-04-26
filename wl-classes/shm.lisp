370037662d0dfb7b6b9fdc6b7c5ed0f46677d0e5
bmiww <bmiww@bky.one>
2024-04-26 02:20:49 +0200

    Added the SHM global bind. Notifying color formats

--- wl-classes/shm.lisp
+++ wl-classes/shm.lisp
@@ -1,5 +1,17 @@
 (in-package :smuks)
 
+(defclass shm-global (wl-shm:global)
+  ())
+
+(defmethod wl-shm:dispatch-bind :after ((global wl-shm:global) client data version id)
+  (declare (ignore global data version))
+  (let* ((interface (wl:iface client id)))
+    ;; TODO: replace the hardcoded numbers with actual parsed enumy values
+    (wl-shm:send-format interface 0)
+    (wl-shm:send-format interface 1)))
+
+
+
 (defclass shm (wl-shm:dispatch)
   ())
 
