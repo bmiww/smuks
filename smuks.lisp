724842f806a5cda147171047a0fc15aaeb24ce00
bmiww <bmiww@bky.one>
2024-04-30 16:09:06 +0200

    Reordered things in the main file. New restart

--- smuks.lisp
+++ smuks.lisp
@@ -52,6 +52,7 @@
   ;; TODO: Turn this into a poller also
   (when *client-poller* (cl-async:free-poller *client-poller*) (setf *client-poller* nil))
   (when *drm-poller* (cl-async:free-poller *drm-poller*) (setf *drm-poller* nil))
+  (when *wl-poller* (cl-async:free-poller *wl-poller*) (setf *wl-poller* nil))
   (cleanup-egl)
   (cleanup-drm))
 
@@ -63,21 +64,60 @@
     (close-drm *drm-dev* *frame-buffer* *buffer-object*)
     (setf *drm-dev* nil) (setf *frame-buffer* nil) (setf *buffer-object* nil)))
 
-(defun main-after-drm ()
+(defun recursively-render-frame ()
+  (if *smuks-exit*
+      (cl-async:exit-event-loop)
+      (progn
+	(render-frame)
+	;; (do-nothing)
+	(cl-async:delay 'recursively-render-frame :time 0.016))))
+
+(defun init-shaders ()
+  ;; TODO: Is this main-vbo still used?
+  (setf *main-vbo* (prep-gl-implementation *drm-dev* *frame-buffer*))
+  (setf *rect-shader* (shader-init:create-rect-shader *drm-dev*))
+  (setf *texture-shader* (shader-init:create-texture-shader *drm-dev*)))
+
+(defun init-globals ()
+  ;; TODO: Also iterate and generate globals for outputs here
+  ;; TODO: When you recompile the compiled classes - these globals aren't updated
+  ;; needing a rerun
+  (make-instance 'wl-compositor:global :display *wayland* :dispatch-impl 'compositor)
+  (make-instance 'wl-subcompositor:global :display *wayland*)
+  (make-instance 'wl-shm:global :display *wayland* :dispatch-impl 'shm)
+  (make-instance 'wl-seat:global :display *wayland*)
+  (make-instance 'wl-data-device-manager:global :display *wayland*)
+  (make-instance 'xdg-wm-base:global :display *wayland* :dispatch-impl 'wm-base))
+
+(defun mainer ()
+  (setf *log-output* *standard-output*)
+  (heading)
+
+  (setf (uiop/os:getenv "WAYLAND_DEBUG") "1")
+  (setf *socket* (init-socket))
+
+  ;; TODO: Can sometimes fail on retrying
+  (setf *drm-dev* (init-drm))
+
+  (wl:init-interface-definitions)
+  (setf *wayland* (wl:display-create))
+  (wl:display-add-socket-fd *wayland* (unix-sockets::fd *socket*))
+
+  (setf *wl-event-loop* (wl:display-get-event-loop *wayland*))
+  (setf *wl-event-fd* (wl:event-loop-get-fd *wl-event-loop*))
+
+  (init-globals)
+
   (setf (values *egl* *egl-context*) (init-egl *drm-dev* *wayland*))
 
   (setf (values *frame-buffer* *egl-image* *buffer-object*) (create-framebuffer *egl* *drm-dev*))
   (setf (values *gl-frame-buffer* *texture*) (create-gl-framebuffer *egl-image*))
 
-  (setf *main-vbo* (prep-gl-implementation *drm-dev* *frame-buffer*))
-  (setf *rect-shader* (shader-init:create-rect-shader *drm-dev*))
-  (setf *texture-shader* (shader-init:create-texture-shader *drm-dev*))
+  (init-shaders)
 
   (unless *active-crtc* (setf *active-crtc* (set-crtc *drm-dev* *frame-buffer*)))
 
   (setf (uiop/os:getenv "WAYLAND_DISPLAY") *socket-file*)
-
-  ;; (test-app *test-program*)
 
   (livesupport:continuable
     (cl-async:start-event-loop
@@ -96,47 +136,13 @@
   (setf *smuks-exit* nil)
   (cleanup))
 
-(defun recursively-render-frame ()
-  (if *smuks-exit*
-      (cl-async:exit-event-loop)
-      (progn
-	(render-frame)
-	;; (do-nothing)
-	(cl-async:delay 'recursively-render-frame :time 0.016))))
-
-(defun do-nothing ()
-  (livesupport:update-repl-link)
-  (flush-clients))
-
 (defun main ()
-  (setf *log-output* *standard-output*)
-  (heading)
+  (restart-case (mainer)
+    (start-over () (cleanup) (mainer))))
 
-  (setf (uiop/os:getenv "WAYLAND_DEBUG") "1")
-  (setf *socket* (init-socket))
-
-  ;; TODO: Can sometimes fail on retrying
-  (setf *drm-dev* (init-drm))
-
-  (wl:init-interface-definitions)
-  (setf *wayland* (wl:display-create))
-  (wl:display-add-socket-fd *wayland* (unix-sockets::fd *socket*))
-
-  (setf *wl-event-loop* (wl:display-get-event-loop *wayland*))
-  (setf *wl-event-fd* (wl:event-loop-get-fd *wl-event-loop*))
-
-  ;; TODO: Also iterate and generate globals for outputs here
-  ;; TODO: When you recompile the compiled classes - these globals aren't updated
-  ;; needing a rerun
-  (make-instance 'wl-compositor:global :display *wayland* :dispatch-impl 'compositor)
-  (make-instance 'wl-subcompositor:global :display *wayland*)
-  (make-instance 'wl-shm:global :display *wayland* :dispatch-impl 'shm)
-  (make-instance 'wl-seat:global :display *wayland*)
-  (make-instance 'wl-data-device-manager:global :display *wayland*)
-  (make-instance 'xdg-wm-base:global :display *wayland* :dispatch-impl 'wm-base)
-
-  (restart-case (main-after-drm)
-    (retry () (cleanup-egl) (main-after-drm) )))
+;; ┌─┐┬─┐┌─┐┌┬┐┌─┐
+;; ├┤ ├┬┘├─┤│││├┤
+;; └  ┴└─┴ ┴┴ ┴└─┘
 
 ;; egl.shaders.texture_abgr.as_mut().expect("Texture program not found")
 ;; .draw(
@@ -156,22 +162,26 @@
     (mapcar (lambda (surface) (render-surface surface)) surfaces)))
 
 
+(defun do-nothing ()
+  (livesupport:update-repl-link)
+  (wl:display-flush-clients *wayland*))
 
 (defun render-frame ()
   (livesupport:update-repl-link)
   (gl:bind-framebuffer :framebuffer *gl-frame-buffer*)
   (gl:clear :color-buffer-bit)
-  (shaders.rectangle:draw *rect-shader* `(,(shaders.rectangle::make-rect :x 10.0 :y 300.0 :w 100.0 :h 40.0
-									 :color '(0.2 0.2 0.2 1.0))))
-  (shaders.rectangle:draw *rect-shader* `(,(shaders.rectangle::make-rect :x 30.0 :y 500.0 :w 200.0 :h 50.0
-									 :color '(0.2 0.9 0.2 1.0))))
+
+  (shaders.rectangle:draw *rect-shader* `(,(shaders.rectangle::make-rect
+					    :x 10.0 :y 300.0 :w 100.0 :h 40.0
+					    :color '(0.2 0.2 0.2 1.0))))
+
+  (shaders.rectangle:draw *rect-shader* `(,(shaders.rectangle::make-rect
+					    :x 30.0 :y 500.0 :w 200.0 :h 50.0
+					    :color '(0.2 0.9 0.2 1.0))))
 
   (render-clients)
   (gl:flush)
   (gl:finish)
-  (flush-clients))
-
-(defun flush-clients ()
   (wl:display-flush-clients *wayland*))
 
 ;; ┬  ┬┌─┐┌┬┐┌─┐┌┐┌┌─┐┬─┐┌─┐
