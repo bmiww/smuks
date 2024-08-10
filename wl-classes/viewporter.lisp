7596a07d1c2f2f30fb5f7ceb1e3bd87bc6dabd31
bmiww <bmiww@bky.one>
2024-08-10 18:36:33 +0200

    Still unused viewport destination implementation

--- wl-classes/viewporter.lisp
+++ wl-classes/viewporter.lisp
@@ -19,4 +19,15 @@
 ;;  └┘ ┴└─┘└┴┘┴  └─┘┴└─ ┴
 
 (defclass viewport (wp-viewport:dispatch)
-  ((surface :initarg :surface :accessor surface)))
+  ;; TODO: You wanted to make a surface inherit the viewport class instead
+  ((surface :initarg :surface :accessor surface)
+   (view-x :initform nil :accessor view-x)
+   (view-y :initform nil :accessor view-y)
+   (view-w :initform nil :accessor view-w)
+   (view-h :initform nil :accessor view-h)
+   (view-dest-h :initform nil :accessor view-dest-h)
+   (view-dest-w :initform nil :accessor view-dest-w)))
+
+(defmethod wp-viewport:set-destination ((viewport viewport) w h)
+  (setf (view-dest-w viewport) w
+	(view-dest-h viewport) h))
